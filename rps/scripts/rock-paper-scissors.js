6ab474393d68c75f6df7dcab52ce5c1ef0d91af6
Michelle <rozaydoesntlovexo@Rozaydoesntloves-MacBook-Pro.local>
2024-06-03 15:10:45 -0400

    adding autoplay

--- rps/scripts/rock-paper-scissors.js
+++ rps/scripts/rock-paper-scissors.js
@@ -17,6 +17,23 @@ ties: 0
 };
 }
 */
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay(){
+    if(!isAutoPlaying){
+   intervalId =  setInterval(function(){
+      const playerMove = pickComputerMove();
+    playGame(playerMove);
+     }, 1000 );
+     isAutoPlaying = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
 function playGame(playerMove){
 const computerMove = pickComputerMove();
 
